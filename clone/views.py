06ba62b3ddca26253ddb9d9e99360ee75d1a65aa
candycrushpro <bnilmar54@gmail.com>
2020-10-19 00:19:19 +0300

    Post view function

--- clone/views.py
+++ clone/views.py
@@ -17,6 +17,20 @@ def homepage (request):
     return render(request,'home.html',locals())
 
 @login_required(login_url='/accounts/login')
+def post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImage(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('homePage')
+    else:
+        form = NewImage()
+    return render(request, 'post.html',locals())    
+
+@login_required(login_url='/accounts/login')
 def profile(request):
     current_user = request.user
     if request.method == 'POST':
@@ -28,3 +42,17 @@ def profile(request):
         else:
             form=UpdateProfile()
     return render(request, 'profile/new.html',locals())
+
+@login_required(login_url='/accounts/login')
+def showprofile(request,username):
+     profile = User.objects.get(username=username)
+     print(profile)
+     try:
+        profile_details = Profile.get_by_id(profile.id)
+        print(profile_details)
+     except:
+        profile_details = Profile.filter_by_id(profile.id)
+        print(profile_details)
+     images = Image.profile_images(profile.id)
+     print(images)
+     return render(request, 'profile/profile.html', {'profile':profile, 'profile_details':profile_details, 'images':images})    

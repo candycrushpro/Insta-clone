91a20a915d810da878922c328f021e5b4ac836c6
candycrushpro <bnilmar54@gmail.com>
2020-10-19 00:46:03 +0300

    install pillow

--- clone/models.py
+++ clone/models.py
@@ -43,7 +43,7 @@ class Image(models.Model):
       image_caption = HTMLField()
       post_date = models.DateTimeField(auto_now_add=True)
       user = models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
-      profile_details=models.ForeignKey(Profile)
+      profile_details=models.ForeignKey(Profile,on_delete=models.CASCADE)
       
 
       def __str__(self):
@@ -76,7 +76,7 @@ class Image(models.Model):
 class Comments(models.Model):
     comment=models.TextField(max_length=50)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    image=models.ForeignKey(Image,blank=True)
+    image=models.ForeignKey(Image,blank=True,on_delete=models.CASCADE)
     def __str__(self):
         return self.comment
     
--- clone/urls.py
+++ clone/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.static import static
 
 urlpatterns=[
   path('',views.homepage,name='homePage'),
-#   url(r'^post',views.post,name='post'),
+  path('post/',views.post,name='post'),
   path('profile/',views.profile,name='profile'),
-#   url(r'^user/(?P<username>\w+)',views.showprofile,name ='showprofile'),
-#   url(r'^ search/',views.search, name='search'),
-#   url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+  path('user/(?P<username>\w+)',views.showprofile,name ='showprofile'),
+  path('search/',views.search, name='search'),
+  path('comment/(?P<image_id>\d+)', views.comment, name='comment'),
 ]
-# if settings.DEBUG:
-    # urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
